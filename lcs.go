985b06d44a78c1acf69565720d1d4eb102e58ff8
“psykhi” <alexandre.pesant@gmail.com>
2019-02-09 15:29:20 +0100

    Longest common subsequence

---
+++ lcs.go
@@ -0,0 +1,63 @@
+package sequencing
+
+func LCSStrings(a []string, b []string, v0 []int, v1 []int) int {
+	m := len(a)
+	n := len(b)
+
+	if v0 == nil {
+		v0 = make([]int, n+1, n+1)
+	}
+	if v1 == nil {
+		v1 = make([]int, n+1, n+1)
+	}
+
+	for i := 0; i < n+1; i++ {
+		v0[i] = i
+	}
+
+	for i := 0; i < m; i++ {
+		v1[0] = i + 1
+
+		for j := 0; j < n; j++ {
+			if a[i] == b[j] {
+				v1[j+1] = v0[j] + 1
+			} else {
+				v1[j+1] = max(v1[j], v0[j+1])
+			}
+		}
+		temp := v0
+		v0 = v1
+		v1 = temp
+	}
+
+	return v0[n]
+}
+
+func LCS(a []byte, b []byte, v0 []int, v1 []int) int {
+	m := len(a)
+	n := len(b)
+
+	if v0 == nil {
+		v0 = make([]int, n+1, n+1)
+	}
+	if v1 == nil {
+		v1 = make([]int, n+1, n+1)
+	}
+
+	for i := 0; i < m; i++ {
+		v1[0] = 0
+
+		for j := 0; j < n; j++ {
+			if a[i] == b[j] {
+				v1[j+1] = v0[j] + 1
+			} else {
+				v1[j+1] = max(v1[j], v0[j+1])
+			}
+		}
+		temp := v0
+		v0 = v1
+		v1 = temp
+	}
+
+	return v0[n]
+}
---
+++ lcs_test.go
@@ -0,0 +1,17 @@
+package sequencing
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLCS(t *testing.T) {
+	d := LCS([]byte("kitten"), []byte("sitting"), nil, nil)
+	assert.Equal(t, 4, d)
+
+	d = LCS([]byte("MZJAWXU"), []byte("XMJYAUZ"), nil, nil)
+	assert.Equal(t, 4, d)
+
+	d = LCS([]byte("AGCAT"), []byte("GAC"), nil, nil)
+	assert.Equal(t, 2, d)
+}
